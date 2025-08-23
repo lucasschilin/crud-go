b30c6afd8c7943aa894b2863528f6e9769ae2acb
Lucas Schilin <schilin.lucas@gmail.com>
2025-08-23 20:14:39 -0300

    feat: setup application error handling pattern in rest_err package

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # crud-go
+
+### Technologies
+- GO version = v1.24
+- Arquitecture pattern = MVC
+
---
+++ configuration/rest_err/rest_err.go
@@ -0,0 +1,52 @@
+package rest_err
+
+import (
+	"net/http"
+)
+
+type RestErr struct {
+	Message string  `json:"message"`
+	Err     string  `json:"error"`
+	Code    int64   `json:"code"`
+	Causes  []Cause `json:"causes"`
+}
+
+type Cause struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+func NewRestErr(message, err string, code int64, causes []Cause) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return NewRestErr(message, "bad_request", http.StatusBadRequest, nil)
+}
+
+func NewBadRequestValidationError(message string, causes []Cause) *RestErr {
+	return NewRestErr(message, "bad_request", http.StatusBadRequest, causes)
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return NewRestErr(
+		message, "internal_server_error", http.StatusInternalServerError, nil,
+	)
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return NewRestErr(message, "not_found", http.StatusNotFound, nil)
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return NewRestErr(message, "forbidden", http.StatusForbidden, nil)
+}
