cb88fce4ba137dc11c6905b32616b383faa32184
Lucas Schilin <schilin.lucas@gmail.com>
2025-08-31 00:06:34 -0300

    chore: complete user repository setup
    pendings:
    - create tests

--- src/model/repository/create_user_repository.go
+++ src/model/repository/create_user_repository.go
@@ -1,13 +1,37 @@
 package repository
 
 import (
+	"context"
+	"os"
+
+	"github.com/lucasschilin/crud-go/src/configuration/logger"
 	"github.com/lucasschilin/crud-go/src/configuration/rest_err"
 	"github.com/lucasschilin/crud-go/src/model"
+)
+
+const (
+	COLLECTION_NAME = "DATABASE_USER_COLLECTION_NAME"
 )
 
 func (ur *userRepository) CreateUser(
 	userDomain model.UserDomainInterface,
 ) (model.UserDomainInterface, *rest_err.RestErr) {
-	//
-	return nil, nil
+
+	logger.Info("Init createUser repository")
+
+	collection := ur.databaseConnection.Collection(os.Getenv(COLLECTION_NAME))
+
+	userDomainVal, err := userDomain.GetJSONValue()
+	if err != nil {
+		return nil, rest_err.NewInternalServerError(err.Error())
+	}
+
+	result, err := collection.InsertOne(context.Background(), userDomainVal)
+	if err != nil {
+		return nil, rest_err.NewInternalServerError(err.Error())
+	}
+
+	userDomain.SetID(result.InsertedID.(string))
+
+	return userDomain, nil
 }
--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -3,16 +3,23 @@ package model
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
 )
 
 type userDomain struct {
-	email    string
-	password string
-	name     string
-	age      int8
+	ID       string
+	Email    string `json:"email"`
+	Password string
+	Name     string
+	Age      int8
 }
 
 type UserDomainInterface interface {
+	SetID(string)
+
+	GetJSONValue() (string, error)
+
 	GetEmail() string
 	GetName() string
 	GetPassword() string
@@ -26,30 +33,47 @@ func NewUserDomain(
 	age int8,
 ) UserDomainInterface {
 	return &userDomain{
-		email, password, name, age,
+		Email:    email,
+		Password: password,
+		Name:     name,
+		Age:      age,
 	}
 }
 
+func (ud *userDomain) SetID(id string) {
+	ud.ID = id
+}
+
+func (ud *userDomain) GetJSONValue() (string, error) {
+	b, err := json.Marshal(ud)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	return string(b), nil
+}
+
 func (ud *userDomain) GetEmail() string {
-	return ud.email
+	return ud.Email
 }
 
 func (ud *userDomain) GetName() string {
-	return ud.name
+	return ud.Name
 }
 
 func (ud *userDomain) GetPassword() string {
-	return ud.password
+	return ud.Password
 }
 
 func (ud *userDomain) GetAge() int8 {
-	return ud.age
+	return ud.Age
 }
 
 func (ud *userDomain) EncryptPassword() {
 	hash := md5.New()
 	defer hash.Reset()
 
-	hash.Write([]byte(ud.password))
-	ud.password = hex.EncodeToString(hash.Sum(nil))
+	hash.Write([]byte(ud.Password))
+	ud.Password = hex.EncodeToString(hash.Sum(nil))
 }
